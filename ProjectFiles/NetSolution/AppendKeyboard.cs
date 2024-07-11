9d522fd5dc3006530cb3a471ca5146808aeff9d5
MassimoV <massimo.variolo@rockwellautomation.com>
2024-07-11 13:26:50 +0200

    managing floating point

--- ProjectFiles/NetSolution/AppendKeyboard.cs
+++ ProjectFiles/NetSolution/AppendKeyboard.cs
@@ -16,30 +16,48 @@ using FTOptix.Store;
 
 public class AppendKeyboard : BaseNetLogic
 {
+    private IUAVariable typingDecimals;
+    private int numberOfDecimals;
     private SpinBox sBox;
 
     public override void Start()
     {
+        typingDecimals = LogicObject.GetVariable("typingDecimals");
+        numberOfDecimals = 0;
         sBox = ((SpinBox)Owner);
         Clear();
-    }
-
-    public override void Stop()
-    {
-        // Insert code to be executed when the user-defined logic is stopped
     }
 
     [ExportMethod]
     public void AppendValue(string value) 
     {
-        var tempValue = sBox.Value.ToString() + value;
-        sBox.Value = float.Parse(tempValue);
+        var sboxValue = sBox.Value.ToString();
+        var newValue = int.Parse(value);
+        var sboxValueHasDecimals = sboxValue.Contains(',');
+        var integerPart = sboxValue.Split(',')[0];
+        var decimalPart = sboxValueHasDecimals ? sboxValue.Split(',')[1].Substring(0, numberOfDecimals) : string.Empty;
+        var res = float.Parse(value);
+
+        if ((bool) typingDecimals.Value.Value && !sboxValueHasDecimals)
+        {
+            res = int.Parse(sBox.Value + value) * 0.1f;
+            numberOfDecimals++;
+        } else if (sboxValueHasDecimals)
+        {
+            numberOfDecimals++;
+            decimalPart += value;
+            res = (float) (int.Parse(integerPart + decimalPart) * Math.Pow(0.1, numberOfDecimals));
+        } 
+
+        sBox.Value = res;
+        typingDecimals.Value = false;
     }
 
     [ExportMethod]
     public void Clear()
     {
         sBox.Value = 0;
+        numberOfDecimals = 0;
     }
 
     [ExportMethod]
@@ -48,9 +66,17 @@ public class AppendKeyboard : BaseNetLogic
         sBox.Value *= -1;
     }
 
+
+
     [ExportMethod]
     public void Backspace()
     {
+        var sboxValueHasDecimals = sBox.Value.ToString().Contains(',');
+        if (sboxValueHasDecimals )
+        {
+            numberOfDecimals--;
+        }
+
         var sBoxLength = sBox.Value.ToString().Length;
         if (sBoxLength == 1) return;
         var tempValue = sBox.Value.ToString().Substring(0, sBoxLength - 1);
